b226c74c0fea8c6590c307bfb08cf37772bfe2a5
Sarthak Sarkar <22SSarkar@heckgrammar.co.uk>
2025-04-04 10:06:48 +0100

    yjt

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            while (true) 
+            {
+                DateTime rightnow = DateTime.Now;
+                DateTime target = new DateTime(2025, 4, 4, 15, 15, 0);
+                TimeSpan timeleft = target - rightnow;
+                Console.WriteLine(timeleft);
+                Thread.Sleep(1000);
+            }
         }
     }
 }
